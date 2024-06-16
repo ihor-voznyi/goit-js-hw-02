6197d79fefc86966b4e13e648053afe91edb07aa
ihor-voznyi <haiku.kiev@gmail.com>
2024-06-17 00:32:16 +0300

    upd tsk-2
    2upd

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,6 @@
-function formatMessage(maxLength, message) [
+function formatMessage(maxLength, message) {
     return message.length <= maxLength ? message : `${message.slice(0, maxLength)}...`;
-]
+}
 
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
 console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
