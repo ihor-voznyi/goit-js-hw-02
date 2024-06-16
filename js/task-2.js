bb8bea65c6dd01cda8260f5e791d8202dae34795
ihor-voznyi <haiku.kiev@gmail.com>
2024-06-16 21:18:08 +0300

    Update task-2.js
    upd

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,10 @@
-function(maxLength, message)
+function formatMessage(maxLength, message) [
+    return message.length <= maxLength ? message : `${message.slice(0, maxLength)}...`;
+]
+
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis...""
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis...""
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
